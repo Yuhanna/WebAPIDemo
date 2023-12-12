75d44b6fd3dd07fc53406e1d4898908d3758ec81
Yuhanna <yuhannaergun@yahoo.com>
2023-12-12 09:49:22 +0300

    ROM_Database

--- WebAPIDemo/Data/ApplicationDbContext.cs
+++ WebAPIDemo/Data/ApplicationDbContext.cs
@@ -16,7 +16,7 @@ namespace WebAPIDemo.Data
             new Shirt { ShirtId = 1, Brand = "My Brand", Color = "Blue", Gender = "Man", Price = 30, Size = 10 },
             new Shirt { ShirtId = 2, Brand = "My Brand1", Color = "Blue1", Gender = "Man1", Price = 130, Size = 110 },
             new Shirt { ShirtId = 3, Brand = "My Brand2", Color = "Blue2", Gender = "Man2", Price = 230, Size = 210 },
-            new Shirt { ShirtId = 4, Brand = "My Brand3", Color = "Blue3", Gender = "Man3", Price = 330, Size = 310 },
+            new Shirt { ShirtId = 4, Brand = "My Brand3", Color = "Blue3", Gender = "Man3", Price = 330, Size = 310 }
                 );
         }
     }
