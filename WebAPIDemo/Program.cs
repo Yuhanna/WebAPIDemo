9c4fbbc3c7bd2d821230992d81018c94e7ea422a
Yuhanna <yuhannaergun@yahoo.com>
2023-12-18 16:59:40 +0300

    swagger

--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -13,9 +13,24 @@ builder.Services.AddDbContext<ApplicationDbContext>(Options =>
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();//For Swagger Document
+builder.Services.AddSwaggerGen(c =>
+    {
+      //  c.OperationFilter<AuthorizationHeaderOperationFilter>
+});////For Swagger Document
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
+if(app.Environment.IsDevelopment())//For Swagger Document
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 
 app.UseHttpsRedirection();
 
